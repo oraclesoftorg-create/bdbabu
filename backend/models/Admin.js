1b1b139ca6e7805216793341c888ff0850ec540a
shihab <shihab@gmail.com>
2026-06-30 10:58:48 +0600

    first commit

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const adminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true,
